2077eb03074c6ce8b914b0150e085719eaceb662
Winston Chen <winstonchen.wm@gmail.com>
2021-02-16 10:18:23 -0500

    first draft

--- wander-plus-backend/config/routes.rb
+++ wander-plus-backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/locations', to: 'locations#index' 
+  get '/reviews', to: 'reviews#index'
+  get '/locations/:id', to: 'locations#show'
 end
