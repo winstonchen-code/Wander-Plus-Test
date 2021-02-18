c93658acd3a65a57443e98801bfcf463f3bf6a59
Winston Chen <winstonchen.wm@gmail.com>
2021-02-18 11:37:08 -0500

    third draft

--- wander-plus-frontend-draft/src/components/Location/Location.js
+++ wander-plus-frontend-draft/src/components/Location/Location.js
@@ -55,13 +55,16 @@ function Location(props) {
         
         // const location_id = location.id
         review.location_id = location.id
-        review.user_id = 4
+        review.user_id = 5
         // console.log(review)
         axios.post('http://localhost:3000/reviews', {review})
         .then(resp => {
-            const included = [...location.reviews, resp.data ]
-            console.log(included)
-            setLocation({...location, included})
+            const newLocation = Object.assign({}, location)
+            // const included = [...location.reviews, resp.data ]
+            newLocation.reviews = [...newLocation.reviews, resp.data]
+            // const included = [...location.reviews, resp.data ]
+            // console.log(included)
+            setLocation(newLocation)
             setReview({title: '', description: '', score: 0})
         })
         .catch(resp => console.log (resp))
@@ -69,13 +72,13 @@ function Location(props) {
 
     const setRating = (score, e) => {
         e.preventDefault()
-        
+
         setReview({...review, score})
     }
 
     let reviews
     if (loaded && location.reviews){
-    reviews = location.reviews.map( ( item, index ) => {
+    reviews = location.reviews.map( ( item, index ) => { console.log(item)
         return(
             <Review
                 key={index} 
--- wander-plus-frontend-draft/src/components/Location/Review.js
+++ wander-plus-frontend-draft/src/components/Location/Review.js
@@ -1,16 +1,37 @@
 import React from 'react'
+import styled from 'styled-components'
+import Rating from'../Rating/Rating'
+
+const Card = styled.div`
+    border: 1px solid rgba(0,0,0,0.1);
+    border-radius: 4px;
+    padding: 20px;
+    margin: 0 20px 20px 0;
+`
+const RatingContainer = styled.div`
+    fisplay:flex;
+    flex-direction: row;
+`
+const Title = styled.div`
+    padding: 20px 0 0 0;
+    font-size: 18px;
+`
+const Description = styled.div`
+    padding: 0 0 20px 0;
+    font-size: 14px;
+`
 
 function Review(props) {
 
     const {score, title, description } = props.attributes
     return (
-        <div className="card">
-            <div className="rating-container">
-                <div className="rating-score">{score}</div>
-            </div>
-            <div className="title">{title}</div>
-            <div className="title">{description}</div>
-        </div>
+        <Card>
+            <RatingContainer>
+                <Rating score={score}/>
+            </RatingContainer>
+            <Title>{title}</Title>
+            <Description>{description}</Description>
+        </Card>
     )
 }
 
